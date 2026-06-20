4bb414eff8b326b5cec46fd2941743b304d53d64
Emre Kadirhan <81781189+ruff1e@users.noreply.github.com>
2026-06-20 16:15:43 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()', '')
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+        if not s:
+            return True
+        return False
+
+
+
+        # s = "([{}])"
+        # s = "[{}]"
+        # s = "[]"
+        # s = ""
