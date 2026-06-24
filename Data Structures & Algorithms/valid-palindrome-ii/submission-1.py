ad314a40f61a7709bd12ca9513f4a2368a292bca
Emre Kadirhan <81781189+ruff1e@users.noreply.github.com>
2026-06-24 17:57:50 -0500

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                skipLeft = s[left + 1 : right + 1]
+                skipRight = s[left : right]
+                return skipLeft == skipLeft[::-1] or skipRight == skipRight[::-1]
+            left += 1
+            right -= 1
+
+        return True
